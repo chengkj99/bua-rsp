021575a0b49264ae49578e041b5aa84f1d664d94
chengkj99 <ckj121@126.com>
2018-07-16 23:36:15 +0800

    init project

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    data: {{data}}
+    <img src="./assets/logo.png"> data: {{data}}
     <router-view/>
   </div>
 </template>
@@ -10,12 +9,19 @@
 import axios from 'axios'
 export default {
   name: 'App',
-  data () {
+  data() {
     return {
       data: []
     }
   },
-  beforeCreate () {
+  beforeCreate() {
+    axios.get('/api/product/getList').then(
+      res => {
+        this.data = res.data
+      }
+    )
+  },
+  beforeCreate() {
     axios.get('/api/product/getList').then(
       res => {
         this.data = res.data
@@ -26,12 +32,12 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
