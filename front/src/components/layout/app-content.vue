d2e4ceb1ddd16eb071836de1db2850d267132fdd
chengkangjian <chengkangjian@megvii.com>
2019-11-06 17:59:07 +0800

    让分割背景在非主页显示

--- front/src/components/layout/app-content.vue
+++ front/src/components/layout/app-content.vue
@@ -1,19 +1,37 @@
 <template>
   <div class="app-content">
     <router-view class="home" name="home"></router-view>
-    <div class="split-block">
-      <p>厚德笃行   博学尚农</p>
-    </div>
+    <split-block v-if="isShowBg"></split-block>
     <router-view class="main"></router-view>
     <router-view class="login-wrapper" name="login"></router-view>
   </div>
 </template>
 
 <script>
+import SplitBlock from './split-block'
+
 export default {
   name: "app-content",
+  components: {
+    SplitBlock
+  },
   data() {
-    return {};
+    return {
+      currentRoutePath: ''
+    };
+  },
+  watch: {
+    $route: {
+      handler(to) {
+        this.currentRoutePath = to.path;
+      },
+      immediate: true
+    }
+  },
+  computed: {
+    isShowBg() {
+      return this.currentRoutePath !== '/home'
+    }
   },
   methods: {}
 };
@@ -32,24 +50,6 @@ export default {
   }
   .login-wrapper {
     background: #f0f4f3;
-  }
-  .split-block {
-    position: relative;
-    height: 100px;
-    width: 100%;
-    background-color: #0f7042;
-    background-image: url("../../assets/img/bg.png");
-    background-repeat: repeat;
-    background-size: cover;
-
-    p {
-      text-align: center;
-      margin: 0;
-      line-height: 100px;
-      font-size: 22px;
-      color: white;
-      word-spacing: 20px;
-    }
   }
 }
 </style>
---
+++ front/src/components/layout/split-block.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="split-block">
+    <p>厚德笃行 博学尚农</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "split-block",
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style lang='less'>
+  .split-block {
+    position: relative;
+    height: 100px;
+    width: 100%;
+    background-color: #0f7042;
+    background-image: url("../../assets/img/bg.png");
+    background-repeat: repeat;
+    background-size: cover;
+
+    p {
+      text-align: center;
+      margin: 0;
+      line-height: 100px;
+      font-size: 22px;
+      color: white;
+      word-spacing: 20px;
+    }
+  }
+</style>
